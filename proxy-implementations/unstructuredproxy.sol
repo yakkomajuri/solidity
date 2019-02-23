b481e80f6c20837dc4f7c6d7d91cff9b017948cb
yakkomajuri <38760734+yakkomajuri@users.noreply.github.com>
2019-02-23 16:33:31 +0000

    Update unstructuredproxy.sol

--- proxy-implementations/unstructuredproxy.sol
+++ proxy-implementations/unstructuredproxy.sol
@@ -1,1 +1,32 @@
+pragma solidity^0.5.0;
 
+contract Proxy  {
+    
+    
+    bytes32 private constant default_location = keccak256(abi.encodePacked("location"));
+    
+    function initialize(address _implementation) public {
+        bytes32 loc = default_location;
+        assembly {
+            sstore(loc, _implementation)
+        }
+    }
+        
+    function() external payable {
+        bytes32 loc = default_location;
+        assembly {
+            let localImpl := sload(loc)
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize)
+            let result := delegatecall(gas, localImpl, ptr, calldatasize, 0, 0)
+            let size := returndatasize
+            returndatacopy(ptr, 0, size)
+
+            switch result
+            case 0 { revert(ptr, size) }
+            default { return(ptr, size) }
+        }
+    }
+        
+        
+    }
