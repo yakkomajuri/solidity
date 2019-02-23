3ab0530be598e1e1321889f4a1ad900ef77d253d
yakkomajuri <38760734+yakkomajuri@users.noreply.github.com>
2019-02-23 16:32:11 +0000

    Update basicproxy.sol

--- proxy-implementations/basicproxy.sol
+++ proxy-implementations/basicproxy.sol
@@ -1,1 +1,15 @@
+ function () public {
+        address localImpl = implementation;
+         //solium-disable-next-line security/no-inline-assembly
+        assembly {
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize)
+            let result := delegatecall(gas, localImpl, ptr, calldatasize, 0, 0)
+            let size := returndatasize
+            returndatacopy(ptr, 0, size)
 
+            switch result
+            case 0 { revert(ptr, size) }
+            default { return(ptr, size) }
+        }
+    }
